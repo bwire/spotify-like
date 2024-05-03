2b5d209b879bdb62a6ea720f745368b12d104d2f
barbedwire <e.barbedwire@gmail.com>
2024-05-03 09:42:48 +0200

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,30 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { DevConfigService } from './common/providers/devConfig.service';
+import { DataSource } from 'typeorm';
 
 const devPort = { port: 3000 };
 const prodPort = { port: 4000 };
 @Module({
-  imports: [SongsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'spotify-like',
+      entities: [],
+      synchronize: true,
+    }),
+    SongsModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
@@ -29,6 +43,9 @@ const prodPort = { port: 4000 };
   ],
 })
 export class AppModule implements NestModule {
+  constructor(private dataSource: DataSource) {
+    console.log('DB name', dataSource.driver.database);
+  }
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes({
       path: 'songs',
