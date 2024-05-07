a9bd8cb0c9574aa0433edc164545938cba678afe
barbedwire <e.barbedwire@gmail.com>
2024-05-07 22:45:54 +0200

    add playlists bits

--- src/playlists/playlists.controller.ts
+++ src/playlists/playlists.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlaylistDto } from './dto/create-playlist.dto';
+import { Playlist } from './playlist.entity';
+import { PlaylistsService } from './playlists.service';
 
 @Controller('playlists')
-export class PlaylistsController {}
+export class PlaylistsController {
+  constructor(private service: PlaylistsService) {}
+
+  @Post()
+  create(@Body() dto: CreatePlaylistDto): Promise<Playlist> {
+    return this.service.create(dto);
+  }
+}
--- src/playlists/playlists.service.ts
+++ src/playlists/playlists.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, Repository } from 'typeorm';
+import { Playlist } from './playlist.entity';
+import { Song } from 'src/songs/song.entity';
+import { User } from 'src/users/user.entity';
+import { CreatePlaylistDto } from './dto/create-playlist.dto';
 
 @Injectable()
-export class PlaylistsService {}
+export class PlaylistsService {
+  constructor(
+    @InjectRepository(Playlist)
+    private playlistsRepository: Repository<Playlist>,
+
+    @InjectRepository(Song)
+    private songsRepository: Repository<Song>,
+
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(dto: CreatePlaylistDto): Promise<Playlist> {
+    const playlist = new Playlist();
+    playlist.name = dto.name;
+    playlist.songs = await this.songsRepository.findBy({ id: In(dto.songs) });
+    playlist.user = await this.usersRepository.findOneBy({ id: dto.user });
+
+    return this.playlistsRepository.save(playlist);
+  }
+}
