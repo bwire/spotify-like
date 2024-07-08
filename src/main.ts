bd8617f434ab2954ac306058c968042ef18d3120
barbedwire <e.barbedwire@gmail.com>
2024-07-08 20:43:13 +0200

    add webpack hot-load

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { SeedService } from './seed/seed.service';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +12,12 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   // const seedService = app.get(SeedService);
   // await seedService.seed();
-
   await app.listen(configService.get<number>('port'));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
